60370235619c97dbef8b7d22b9eb41e796f8e234
Steinar Ágúst Steinarsson <steinar.a.steinarsson@landsbankinn.is>
2021-02-20 18:36:33 +0000

    prófa traina generation

--- c++/Pytorch/mcts_trainer.py
+++ c++/Pytorch/mcts_trainer.py
@@ -2,16 +2,16 @@ import cppyy
 import numpy as np
 import torch
 import torch.tensor as tensor
-from cppyy import ll
 from array import array
 from resnet import ResNet
-from utils import encode_for_predict, encode_for_training
+from utils import encode_for_predict, encode_for_training, decode_training_data
+from train import train_dataset
 from os import listdir
 from os.path import isfile, join
 import re
-#onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 dataset_path = 'data/datasets'
+model_path = 'data/models'
 
 cppyy.include('../Connect4-Game/Game.cpp')
 cppyy.include('../MCTS/TreeNodeLabel.cpp')
@@ -22,14 +22,73 @@ cppyy.include('../MCTS/MCTS.cpp')
 from cppyy.gbl import MCTSAgent
 from cppyy.gbl import Connect4
 
+def predict(values, states, turn, n_states):
+  encoded = encode_for_predict(states, turn, n_states)
+  trainer.predict(encoded, values, n_states)
+
 class MCTS_Trainer:
-  def __init__(self, model, simulations = 200):
-    self.model = model
-    self.clear()
-    self.agent_1 = MCTSAgent.MCTSAgent('AZ_Agent_0', simulations)
-    self.agent_2 = MCTSAgent.MCTSAgent('AZ_Agent_1', simulations)
-    # self.agent_1 = MCTSAgent.MCTSAgent('AZ_Agent_0', simulations, predict)
-    # self.agent_2 = MCTSAgent.MCTSAgent('AZ_Agent_1', simulations, predict)
+  def __init__(self, predictPtr = predict, simulations = 200):
+    self.model = ResNet()
+    self.next_generation(True)
+    # self.agent_1 = MCTSAgent.MCTSAgent('AZ_Agent_0', simulations)
+    # self.agent_2 = MCTSAgent.MCTSAgent('AZ_Agent_1', simulations)
+    self.agent_1 = MCTSAgent.MCTSAgent('AZ_Agent_0', simulations, predictPtr)
+    self.agent_2 = MCTSAgent.MCTSAgent('AZ_Agent_1', simulations, predictPtr)
+
+  def next_generation(self, initializing = False):
+    if initializing:
+      self.generation = 0
+      files = [f for f in listdir(model_path) if isfile(join(model_path, f))]
+      if len(files) != 0:
+        numbers = []
+        for f in files:
+          m = re.search('^model_(\d+)\.pt$', f)
+          numbers.append(int(m.group(1)))
+        self.generation = max(numbers) + 1
+      self.load_model()
+    else:
+      self.generation += 1
+    self.states = []
+    self.turns = []
+    self.policies = []
+    self.values = []
+    self.score = {'AZ_Agent_0': 0, 'AZ_Agent_1': 0, 'Draw': 0}
+
+  # Loads model nr n. If n = -1, then will load latest model
+  def load_model(self, model_n = -1):
+    if self.generation == 0:
+      return False
+    if model_n == -1:
+      model_n = self.generation - 1
+    fpath = model_path + '/model_' + str(model_n) + '.pt'
+    self.model.load_state_dict(torch.load(fpath))
+    return True
+
+  # Saves the latest, newly trained model
+  def save_latest_model(self):
+    fpath = model_path + '/model_' + str(self.generation) + '.pt'
+    torch.save(self.model.state_dict(), fpath)
+
+  # Loads dataset nr n. If n = -1, then will load latest dataset
+  def load_dataset(self, dataset_n = -1):
+    if self.generation == 0:
+      return False
+    if dataset_n == -1:
+      dataset_n = self.generation - 1
+    fpath = dataset_path + '/dataset_' + str(dataset_n) + '.npy'
+    dataset = np.load(fpath, allow_pickle=True)
+    states, turns, policies, values = decode_training_data(dataset)
+    self.states = states
+    self.turns = turns
+    self.policies = policies
+    self.values = values
+    return True
+
+  # Saves the newest dataset
+  def save_latest_dataset(self):
+    data = self.get_training_set()
+    fpath = dataset_path + '/dataset_' + str(self.generation) + '.npy'
+    np.save(fpath, data)
 
   def predict(self, states, values, n_states):
     tensor_arr = tensor(states, dtype=torch.float)
@@ -60,41 +119,22 @@ class MCTS_Trainer:
     return 'Draw'
   
   def play_matches(self, n_matches = 100):
-    for _ in range(n_matches):
+    for m in range(n_matches):
+      should_print = ((m + 1) % 10) == 0
+      if should_print:
+        print('Starting match nr: %d\n' % (m + 1))
       first = self.play_a_game(self.agent_1, self.agent_2)
       second = self.play_a_game(self.agent_2, self.agent_1)
       self.score[first] += 1
       self.score[second] += 1
+  
+  def get_training_set(self):
+    return encode_for_training(self.states, self.turns, self.policies, self.values)
 
-  def clear(self):
-    self.states = []
-    self.turns = []
-    self.policies = []
-    self.values = []
-    self.score = {'AZ_Agent_0': 0, 'AZ_Agent_1': 0, 'Draw': 0}
-
-  def save_dataset(self):
-    file_n = 0
-    files = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f))]
-    if len(files) != 0:
-      numbers = []
-      for f in files:
-        m = re.search('^dataset_(\d+)\.npy$', f)
-        numbers.append(int(m.group(1)))
-      file_n = max(numbers) + 1
-    data = encode_for_training(self.states, self.turns, self.policies, self.values)
-    fpath = dataset_path + '/dataset_' + str(file_n) + '.npy'
-    np.save(fpath, data)
-
-def predict(values, states, turn, n_states):
-  encoded = encode_for_predict(states, turn, n_states)
-  trainer.predict(encoded, values, n_states)
-
-
-model = ResNet()
-trainer = MCTS_Trainer(model)
-trainer.play_matches(1)
-trainer.save_dataset()
-
-
-
+trainer = MCTS_Trainer()
+# trainer.play_matches(1)
+# data = trainer.get_training_set()
+# train_dataset(data, trainer.model, trainer.generation)
+# trainer.save_latest_dataset()
+# trainer.save_latest_model()
+# trainer.next_generation()

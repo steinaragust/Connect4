bd15e185df940138ab19802f548dd20f573878ff
Steinar Ágúst Steinarsson <steinar.a.steinarsson@landsbankinn.is>
2021-03-18 17:04:25 +0000

    WIP

--- c++/BoardGame/play_game.py
+++ c++/BoardGame/play_game.py
@@ -15,14 +15,24 @@ def load_model(model_n):
   model.eval()
   return model
 
-model1 = load_model(7)
-model2 = load_model(7)
-game = cppyy.gbl.Connect4()
-agent1 = MCTSAgent(game.info, 'MCTSAgent1', 200)
-agent2 = MCTSAgent(game.info, 'MCTSAgent2', 200)
+def play_game(agent1, agent2):
+  game.reset()
+  while not game.is_terminal_state():
+    agent = agent1 if game.get_to_move() == 0 else agent2
+    obj = agent.play(game)
+    game.make_move(obj.move)
+    game.print_board()
+    print('\n\n')
+  if not game.winning_move():
+    return "Draw"
+  return agent1.get_name() if game.get_to_move_opponent() == 0 else agent2.get_name()
 
-while not game.is_terminal_state():
-  agent = agent1 if game.get_to_move() else agent2
-  obj = agent.play(game)
-  game.make_move(obj.move)
-  game.print_board()
+model1 = load_model(0)
+model2 = load_model(2)
+game = cppyy.gbl.Connect4()
+agent1 = MCTSAgent(game.info, 'MCTSAgent1', 200, model1)
+agent2 = MCTSAgent(game.info, 'MCTSAgent2', 200, model2)
+winner1 = play_game(agent1, agent2)
+print("Winner match 1: %s" % (winner1))
+winner2 = play_game(agent2, agent1)
+print("Winner match 2: %s" % (winner2))
